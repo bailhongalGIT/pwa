d4b0c1bdcdafd4539723686bb47b9f1a1b21470a
bailhongalgit <bailhongalgit@gmail.com>
2022-08-17 17:48:04 +0530

    share

--- service-worker.js
+++ service-worker.js
@@ -46,7 +46,27 @@ self.addEventListener('fetch', function (event) {
             return response || fetchPromise;
         });
     }));
+
+    const url = new URL(event.request.url);
+    // If this is an incoming POST request for the
+    // registered "action" URL, respond to it.
+    if (event.request.method === 'POST') {
+      event.respondWith((async () => {
+        const formData = await event.request.formData();
+        const link = formData.get('link') || '';
+        const responseUrl = await saveBookmark(link);
+        return Response.redirect(responseUrl, 303);
+      })());
+    }
 });
+
+self.addEventListener('DOMContentLoaded', () => {
+    const parsedUrl = new URL(window.location);
+    // searchParams.get() will properly handle decoding the values.
+    console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
+    console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
+    console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
+  });
 
 self.addEventListener('install', function (event) {
     // The promise that skipWaiting() returns can be safely ignored.
